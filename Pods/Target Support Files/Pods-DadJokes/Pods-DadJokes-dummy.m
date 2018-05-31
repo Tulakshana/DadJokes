694aa6e491c2b0eb46351abe4dc93268bbd5583d
Tula <tula_post@yahoo.com>
2018-05-31 16:44:32 -0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-DadJokes/Pods-DadJokes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DadJokes : NSObject
+@end
+@implementation PodsDummy_Pods_DadJokes
+@end
---
+++ Pods/Target Support Files/Pods-DadJokesTests/Pods-DadJokesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DadJokesTests : NSObject
+@end
+@implementation PodsDummy_Pods_DadJokesTests
+@end
